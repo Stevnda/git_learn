48c2685243c34799d5385e3ae624736ad1a315db
Stevnda <1849698643@qq.com>
2024-03-05 16:51:00 +0800

    commit message

---
+++ test.py
@@ -0,0 +1,18 @@
+'''
+Descripttion: 
+version: 
+Author: mingkang da
+Date: 2024-03-05 16:45:42
+LastEditors: mingkang da
+LastEditTime: 2024-03-05 16:47:14
+'''
+import os
+import numpy
+
+def add(a,b):
+  return a + b
+
+a = 1
+b = 2
+c = add(a,b)
+print(c)
