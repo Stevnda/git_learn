9e2fcfe9886ca20dfaab3f2016975157f75ea757
Stevnda <1849698643@qq.com>
2024-03-05 20:58:35 +0800

    tt

--- test3.py
+++ test3.py
@@ -4,8 +4,9 @@ version:
 Author: mingkang da
 Date: 2024-03-05 18:27:52
 LastEditors: mingkang da
-LastEditTime: 2024-03-05 20:54:59
+LastEditTime: 2024-03-05 20:58:19
 '''
 a = 7890
 b = 456
 c = 789
+d = True
