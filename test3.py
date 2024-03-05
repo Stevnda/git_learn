0afa9a0dbb7a22e8ade6b8f486eabbe54cf8194d
Stevnda <1849698643@qq.com>
2024-03-05 20:53:44 +0800

    dd

--- test3.py
+++ test3.py
@@ -4,7 +4,8 @@ version:
 Author: mingkang da
 Date: 2024-03-05 18:27:52
 LastEditors: mingkang da
-LastEditTime: 2024-03-05 20:51:53
+LastEditTime: 2024-03-05 20:53:16
 '''
 a = 123
 b = 456
+c = 789
