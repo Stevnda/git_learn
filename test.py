24432e89ae1e559d59c60e52c6a415cc1f96b7ba
Stevnda <1849698643@qq.com>
2024-03-05 17:27:50 +0800

    third commit

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ version:
 Author: mingkang da
 Date: 2024-03-05 16:45:42
 LastEditors: mingkang da
-LastEditTime: 2024-03-05 16:47:14
+LastEditTime: 2024-03-05 17:26:06
 '''
 import os
 import numpy
@@ -12,7 +12,7 @@ import numpy
 def add(a,b):
   return a + b
 
-a = 1
+a = 2
 b = 2
 c = add(a,b)
 print(c)
---
+++ test2.py
@@ -0,0 +1,9 @@
+'''
+Descripttion: 
+version: 
+Author: mingkang da
+Date: 2024-03-05 17:27:08
+LastEditors: mingkang da
+LastEditTime: 2024-03-05 17:27:18
+'''
+print('test')
